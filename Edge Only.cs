4f94249c222fb30bdfe73ff3439c2eb6d16bc90c
dapirra <35818038+dapirra@users.noreply.github.com>
2021-01-25 04:07:32 -0500

    Moved the code for the effect to it's own function

--- Edge Only.cs
+++ Edge Only.cs
@@ -1,52 +1,15 @@
 void Render(Surface dst, Surface src, Rectangle rect) {
-    ColorBgra CurrentPixel,
-              TopPixel,
-              BottomPixel,
-              LeftPixel,
-              RightPixel,
-              TopLeftPixel,
-              TopRightPixel,
-              BottomLeftPixel,
-              BottomRightPixel;
     for (int y = rect.Top; y < rect.Bottom; y++) {
         if (IsCancelRequested) return;
         for (int x = rect.Left; x < rect.Right; x++) {
-            // Avoid edges of selection
+            // Avoid edges of the image
             if (x == 0 || y == 0 || x == src.Width - 1 || y == src.Height - 1)
                 continue;
 
-            CurrentPixel = src[x, y];
-            TopPixel = src[x, y - 1];
-            BottomPixel = src[x, y - 1];
-            LeftPixel = src[x - 1, y];
-            RightPixel = src[x + 1, y];
-            TopLeftPixel = src[x - 1, y - 1];
-            TopRightPixel = src[x + 1, y - 1];
-            BottomLeftPixel = src[x - 1, y + 1];
-            BottomRightPixel = src[x + 1, y + 1];
-
-            ColorBgra[] Pixels = {
-                TopPixel,
-                BottomPixel,
-                LeftPixel,
-                RightPixel,
-                TopLeftPixel,
-                TopRightPixel,
-                BottomLeftPixel,
-                BottomRightPixel
-            };
-            bool EraseCurrentPixel = true;
-
-            foreach(ColorBgra Pixel in Pixels) {
-                if (Pixel.A < 255) {
-                    EraseCurrentPixel = false;
-                    break;
-                }
-            }
-
-            if (EraseCurrentPixel) {
+            if (ApplyEffect(src, x, y)) {
+                ColorBgra CurrentPixel = src[x, y];
                 CurrentPixel.A = 0;
-                dst[x,y] = CurrentPixel;
+                dst[x, y] = CurrentPixel;
             }
         }
     }
@@ -54,4 +17,39 @@ void Render(Surface dst, Surface src, Rectangle rect) {
     #if DEBUG
     Debug.WriteLine(DateTime.Now + " - Done");
     #endif
+}
+
+bool ApplyEffect(Surface src, int x, int y) {
+    ColorBgra
+        CurrentPixel = src[x, y],
+        TopPixel = src[x, y - 1],
+        BottomPixel = src[x, y - 1],
+        LeftPixel = src[x - 1, y],
+        RightPixel = src[x + 1, y],
+        TopLeftPixel = src[x - 1, y - 1],
+        TopRightPixel = src[x + 1, y - 1],
+        BottomLeftPixel = src[x - 1, y + 1],
+        BottomRightPixel = src[x + 1, y + 1];
+
+    ColorBgra[] Pixels = {
+        TopPixel,
+        BottomPixel,
+        LeftPixel,
+        RightPixel,
+        TopLeftPixel,
+        TopRightPixel,
+        BottomLeftPixel,
+        BottomRightPixel
+    };
+
+    bool EraseCurrentPixel = true;
+
+    foreach(ColorBgra Pixel in Pixels) {
+        if (Pixel.A < 255) {
+            EraseCurrentPixel = false;
+            break;
+        }
+    }
+
+    return EraseCurrentPixel;
 }
